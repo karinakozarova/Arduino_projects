f09693227db7fe5daf349c25e9172d0391c4e975
Karina Kozarova <karinakozarova1303@gmail.com>
2017-05-13 16:11:53 +0300

    Update Air_quality.ino

--- Air quality/Air_quality.ino
+++ Air quality/Air_quality.ino
@@ -1,9 +1,9 @@
 int sensorValue;
 int digitalValue;
+int previousState = 0; //0 - ok, 1 - bad
 
 int normal_air_diode = 6;
 int bad_air_diode = 13;
-
 /*
    Normal air returns approximately 100-150
    Alcohol returns approximately 700
@@ -12,7 +12,7 @@ int bad_air_diode = 13;
 void setup()
 
 {
-  Serial.begin(9600);    
+  Serial.begin(9600);
   pinMode( 0, INPUT);
   pinMode(bad_air_diode, OUTPUT);
   pinMode(normal_air_diode, OUTPUT);
@@ -22,21 +22,25 @@ void setup()
 void loop()
 
 {
-  
-  sensorValue = analogRead(0);       
+
+  sensorValue = analogRead(0);
   digitalValue = digitalRead(0);
-  //Serial.println(sensorValue, DEC);  // prints the value read
+  Serial.println(sensorValue, DEC);  // prints the value read
   if (sensorValue < 161) {
     // normal air
     Serial.print("It's okay!\n");
     digitalWrite(normal_air_diode, HIGH);
+    digitalWrite(bad_air_diode, LOW);
 
 
-  }else {
+
+  } else {
     Serial.print("Something's wrong!\n");
     digitalWrite(bad_air_diode, HIGH);
+    digitalWrite(normal_air_diode, LOW);
+
 
   }
-  delay(5000);                        
+  delay(5000);
 
 }
